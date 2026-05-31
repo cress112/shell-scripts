7badfb2151b6070e46b026d7f7ce3730efae922d
cress112 <m210d498a75.festa@icloud.com>
2026-05-31 17:45:41 +0900

    feat: 最近の変更

--- bash/commands/yt.sh
+++ bash/commands/yt.sh
@@ -3,56 +3,29 @@
 YT_DIR="${SCRIPT_DIR}/yt-dlp"
 YT_DOWNLOAD_DIR="${HOME}/Downloads"
 
+### ! DL処理がffmpegに流れる場合
+### pip install pycryptodomex
+
 function yt {
     "${YT_DIR}"/yt-dlp.sh "$@"
 }
 
 function yt-list {
-    args=($@)
-
-    if [ -z $args ]; then
-        echo "Usage: yt-list <url> [yt-options]"
-        return 1
-    fi
-
-    yt "${args[0]}" --list-formats --skip-download "${args[@]:1}"
+    yt --list-formats --skip-download "$@"
 }
 
 function yt-dl {
-    args=($@)
-
-    if [ -z $args ]; then
-        echo "Usage: yt-dl <url> [yt-options]"
-        echo "in most cases, you should specify format"
-        return 1
-    fi
-
-    yt "${args[0]}" --output "${YT_DOWNLOAD_DIR}"/'%(upload_date)s_%(title)s.%(ext)s' --write-thumbnail "${args[@]:1}"
+    yt --output "${YT_DOWNLOAD_DIR}"/'%(upload_date)s_%(title)s.%(ext)s' --write-thumbnail "$@"
 }
 
 function ytjs {
-    "${YT_DIR}"/yt-dlp.sh "$@" --js-runtimes node:$(which node) --remote-components ejs:github
+    "${YT_DIR}"/yt-dlp.sh --js-runtimes node:$(which node) --remote-components ejs:github "$@"
 }
 
 function ytjs-list {
-    args=($@)
-
-    if [ -z $args ]; then
-        echo "Usage: yt-list <url> [yt-options]"
-        return 1
-    fi
-
-    ytjs "${args[0]}" --list-formats --skip-download "${args[@]:1}"
+    ytjs --list-formats --skip-download "$@"
 }
 
 function ytjs-dl {
-    args=($@)
-
-    if [ -z $args ]; then
-        echo "Usage: yt-dl <url> [yt-options]"
-        echo "in most cases, you should specify format"
-        return 1
-    fi
-
-    ytjs "${args[0]}" --output "${YT_DOWNLOAD_DIR}"/'%(upload_date)s_%(title)s.%(ext)s' --write-thumbnail "${args[@]:1}"
+    ytjs --output "${YT_DOWNLOAD_DIR}/%(upload_date)s_%(title)s.%(ext)s" "$@"
 }
